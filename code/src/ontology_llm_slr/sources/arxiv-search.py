9a25a0eef772c33dc20bf9f2c7423b059ae01141
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 16:36:51 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -8,4 +8,5 @@ import arxiv2bib
 client = arxiv.Client()
 search = arxiv.Search(query="all:ontolog* AND (all:LLM* OR all:language model*)")
 first_result = next(client.results(search))
+print(first_result)
 print(arxiv2bib.arxiv2bib([first_result]))
