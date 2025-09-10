877d13a1aaa48997c66905b245c4a3c9df7cb2da
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 14:50:53 +0200

    code/src/ontology_llm_slr/sources/acl-anthology.py

--- code/src/ontology_llm_slr/sources/acl-anthology.py
+++ code/src/ontology_llm_slr/sources/acl-anthology.py
@@ -3,6 +3,7 @@
 """Querying ACL Anthology for papers"""
 
 import re
+
 from acl_anthology import Anthology
 
 anthology = Anthology.from_repo()
@@ -10,6 +11,7 @@ anthology = Anthology.from_repo()
 for paper in anthology.papers():
     title = str(paper.title).lower()
     abstract = str(paper.abstract).lower()
-    if re.search(r"ontolog*", title) and (re.search(r"llm*", title) or re.search(r"language model*", title)):
-        
-    
+    if re.search(r"ontolog*", title) and (
+        re.search(r"llm*", title) or re.search(r"language model*", title)
+    ):
+        pass
