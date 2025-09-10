48a43e3af3c115ec9bc9a0eb0734d688b45e0bad
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 16:38:02 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -3,11 +3,12 @@
 """Extracting Papers from ArXiv"""
 
 import os
+
 import arxiv
 import arxiv2bib
 
 client = arxiv.Client()
 search = arxiv.Search(query="all:ontolog* AND (all:LLM* OR all:language model*)")
 first_result = next(client.results(search))
-idx = 
-print(arxiv2bib.arxiv2bib([str(first_result)]))
+idx = os.path.basename(str(first_result))
+print(arxiv2bib.arxiv2bib([idx]))
