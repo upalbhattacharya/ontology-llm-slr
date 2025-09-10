95124717ab5857ee82cb30165a822a3d4c863e63
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 14:51:35 +0200

    code/src/ontology_llm_slr/sources/acl-anthology.py

--- code/src/ontology_llm_slr/sources/acl-anthology.py
+++ code/src/ontology_llm_slr/sources/acl-anthology.py
@@ -11,7 +11,8 @@ anthology = Anthology.from_repo()
 for paper in anthology.papers():
     title = str(paper.title).lower()
     abstract = str(paper.abstract).lower()
-    if re.search(r"ontolog*", title) and (
-        re.search(r"llm*", title) or re.search(r"language model*", title)
-    ):
+    if (re.search(r"ontolog*", title) and (
+        re.search(r"llm*", title) or re.search(r"language model*", title))) or
+        (re.search(r"ontolog*", abstract) and (
+        re.search(r"llm*", abstract) or re.search(r"language model*", abstract))):
         pass
