f7b3eea6f0c3c48d2e60dc2c60dca591784836a5
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 17:02:03 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -12,10 +12,11 @@ client = arxiv.Client()
 search = arxiv.Search(query="all:ontolog* AND (all:LLM* OR all:language model*)")
 ids = []
 
-for result in tqdm.tqdm(client.results(search)):
-    ids.append(os.path.basename(str(result)))
-
-with open(
-    "/home/upal/Projects/ontology-llm-slr/code/sources/parts/arxiv/id_list.txt", "w"
-) as f:
-    f.writelines(ids)
+try:
+    for result in tqdm.tqdm(client.results(search)):
+        ids.append(os.path.basename(str(result)))
+except UnexpectedEmptyPageError e:
+    with open(
+        "/home/upal/Projects/ontology-llm-slr/code/sources/parts/arxiv/id_list.txt", "w"
+    ) as f:
+        f.writelines(ids)
