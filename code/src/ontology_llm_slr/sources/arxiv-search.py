48b81acc9c29af76274865d2cf68e57071afbff7
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 17:05:05 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -19,4 +19,4 @@ except:
     with open(
         "/home/upal/Projects/ontology-llm-slr/code/sources/parts/arxiv/id_list.txt", "w"
     ) as f:
-        f.writelines(idx + " " for idx in ids)
+        f.writelines(idx + "\n" for idx in ids)
