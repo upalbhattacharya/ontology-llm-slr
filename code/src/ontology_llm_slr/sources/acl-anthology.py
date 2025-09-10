7606969d43880ee330a616afbfa853275c6337f6
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-10 14:50:47 +0200

    code/src/ontology_llm_slr/sources/acl-anthology.py

--- code/src/ontology_llm_slr/sources/acl-anthology.py
+++ code/src/ontology_llm_slr/sources/acl-anthology.py
@@ -8,4 +8,8 @@ from acl_anthology import Anthology
 anthology = Anthology.from_repo()
 
 for paper in anthology.papers():
+    title = str(paper.title).lower()
+    abstract = str(paper.abstract).lower()
+    if re.search(r"ontolog*", title) and (re.search(r"llm*", title) or re.search(r"language model*", title)):
+        
     
