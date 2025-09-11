a6f397a3607a9ed0abd89475c238d4932a6ba096
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-11 10:54:26 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -9,7 +9,9 @@ import arxiv2bib
 import tqdm
 
 client = arxiv.Client()
-search = arxiv.Search(query='all:ontolog* AND (all:LLM* OR all:"language model"*)')
+search = arxiv.Search(
+    query='all:ontolog* AND all:LLM* OR all:ontolog* AND all:"language model"*'
+)
 ids = []
 fullpaths = []
 
