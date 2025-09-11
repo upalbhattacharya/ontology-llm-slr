e3d43fc1430e44ea82ac6efad79b76f62662884c
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-09-11 10:36:57 +0200

    code/src/ontology_llm_slr/sources/arxiv-search.py

--- code/src/ontology_llm_slr/sources/arxiv-search.py
+++ code/src/ontology_llm_slr/sources/arxiv-search.py
@@ -11,12 +11,20 @@ import tqdm
 client = arxiv.Client()
 search = arxiv.Search(query="all:ontolog* AND (all:LLM* OR all:language model*)")
 ids = []
+fullpaths = []
 
 try:
     for result in tqdm.tqdm(client.results(search)):
+        print(str(result))
+        fullpaths.append(str(result))
         ids.append(os.path.basename(str(result)))
 except:
     with open(
         "/home/upal/Projects/ontology-llm-slr/code/sources/parts/arxiv/id_list.txt", "w"
     ) as f:
         f.writelines(idx + "\n" for idx in ids)
+    with open(
+        "/home/upal/Projects/ontology-llm-slr/code/sources/parts/arxiv/full_paths.txt",
+        "w",
+    ) as f:
+        f.writelines(idx + "\n" for idx in fullpaths)
